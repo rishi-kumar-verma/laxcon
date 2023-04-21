160aac179144c09fbab95b0ca4abf9e927050ab9
Rishi Verma <rishianand001@gmail.com>
2023-04-21 10:16:05 +0530

    api

---
+++ app/Http/Controllers/Api/GradesController.php
@@ -0,0 +1,241 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Grades;
+use App\Models\SubGrades;
+use App\Http\Requests\StoreGradesRequest;
+use App\Http\Requests\UpdateGradesRequest;
+
+class GradesController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $gradesList = Grades::select('id', 'gradeName')->where('status', 1)->get();
+        return $gradesList;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function subGradList()
+    {
+        $subGradesList = SubGrades::where('status', 1)->get();
+        $count = count($subGradesList);
+        $data['status'] = 200;
+        $data['result_cout'] = $count;
+        $data['result'] = $subGradesList;
+        return json_encode($data, true);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function filterSubGradList(StoreGradesRequest $request)
+    {
+
+        $count = 0;
+        $query = [];
+        foreach ($request->all() as $key => $all) {
+            if ($all) {
+                $count = $count + 1;
+            }
+            if ($key != '_token' && $all) {
+                $query[$key] = $all;
+            }
+        }
+        if ($request->subgradeName) {
+            $subGradesList = SubGrades::where('subgradeName', 'LIKE','%'.$request->subgradeName.'%')->get();
+            $count = count($subGradesList);
+            return view('grades', compact(['subGradesList', 'query', 'count']));
+        }
+        $gradesList = [];
+        $gradesListDB = SubGrades::where('status', 1)->get();
+        $firstGradesList = $this->firstList($gradesListDB, $request);
+        foreach ($gradesListDB as $grades) {
+            $check = 0;
+            $arr = json_decode($grades);
+            foreach ($arr as $key => $grade) {
+                if ($key == 'ni' && $request->ni) {
+                    $c = $request->ni;
+                } else if ($key == 'cr' && $request->cr) {
+                    $c = $request->cr;
+                } else if ($key == 'mo' && $request->mo) {
+                    $c = $request->mo;
+                } else if ($key == 'c' && $request->c) {
+                    $c = $request->c;
+                } else if ($key == 's' && $request->s) {
+                    $c = $request->s;
+                } else if ($key == 'p' && $request->p) {
+                    $c = $request->p;
+                } else if ($key == 'si' && $request->si) {
+                    $c = $request->si;
+                } else {
+                    continue;
+                }
+                if (str_contains($grade, 'MAX') || str_contains($grade, 'Max')) {
+                    $c_db = explode(" ", $grade);
+                    if ($c_db[0] >= $c) {
+                        $check = $check + 1;
+                    }
+                } else if (str_contains($grade, '-')) {
+                    $c_db = explode("-", $grade);
+                    if ($c_db[0] < $c  && $c <= $c_db[1]) {
+                        $check = $check + 1;
+                    }
+                } else {
+                    $c_db = $grade;
+                    if ($c_db == $c) {
+                        $check = $check + 1;
+                    }
+                }
+            }
+            if ($check === ($count - 1)) {
+                array_push($gradesList, $grades);
+            }
+        }
+        $subGradesList = [...$firstGradesList, ...$gradesList];
+        $count = count($subGradesList);
+        $data['status'] = 200;
+        $data['result_cout'] = $count;
+        $data['result'] = $subGradesList;
+        $data['query'] = $query;
+
+        return json_encode($data, true);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function firstList($gradesList, $request)
+    {
+        $count = 0;
+        $query = [];
+        foreach ($request->all() as $key => $all) {
+            if ($all) {
+                $count = $count + 1;
+            }
+            if ($key != '_token' && $all) {
+                $query[$key] = $all;
+            }
+        }
+        $subGradesList = [];
+        foreach ($gradesList as $grades) {
+            $check = 0;
+            $arr = json_decode($grades);
+            foreach ($arr as $key => $grade) {
+                if ($key == 'ni' && $request->ni) {
+                    $c = $request->ni;
+                } else if ($key == 'cr' && $request->cr) {
+                    $c = $request->cr;
+                } else if ($key == 'mo' && $request->mo) {
+                    $c = $request->mo;
+                } else if ($key == 'c' && $request->c) {
+                    $c = $request->c;
+                } else if ($key == 's' && $request->s) {
+                    $c = $request->s;
+                } else if ($key == 'p' && $request->p) {
+                    $c = $request->p;
+                } else if ($key == 'si' && $request->si) {
+                    $c = $request->si;
+                } else {
+                    continue;
+                }
+                if (str_contains($grade, '-')) {
+                    $c_db = explode("-", $grade);
+                    if ($c_db[0] == $c) {
+                        $check = $check + 1;
+                    }
+                } else {
+                    $c_db = $grade;
+                    if ($c_db == $c) {
+                        $check = $check + 1;
+                    }
+                }
+            }
+            if ($check === ($count - 1)) {
+                array_push($subGradesList, $grades);
+            }
+        }
+        return $subGradesList;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreGradesRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreGradesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Grades  $grades
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Grades $grades)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Grades  $grades
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Grades $grades)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateGradesRequest  $request
+     * @param  \App\Models\Grades  $grades
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateGradesRequest $request, Grades $grades)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Grades  $grades
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Grades $grades)
+    {
+        //
+    }
+}
